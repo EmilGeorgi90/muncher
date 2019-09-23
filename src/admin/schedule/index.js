61ec9d5d017e2aa68b75394342b29a86ad4b56ed
emil376g <emil376g@edu.campusvejle.dk>
2019-09-23 10:02:50 +0200

    mistake with curly bracket

--- src/admin/schedule/index.js
+++ src/admin/schedule/index.js
@@ -22,7 +22,7 @@ class Schedule {
           for (const url of this.urls) {
             if (url.url !== null) {
               request(
-                `${process.env.MUNCHER_API}/crawl?url=${url.url}&selector=${url.selector.substring(0,1) == '.' ? url.selector.selector : url.selector.selector.substring(1, url.selector.length - 1),
+                `${process.env.MUNCHER_API}/crawl?url=${url.url}&selector=${url.selector.substring(0,1) == '.' ? url.selector.selector : url.selector.selector.substring(1, url.selector.length - 1)
               }`,
                 (error, response, body) => {
                   if (!error && response.statusCode < 400) {
